72749fff4547884401fb5028df788803be14186a
Rodrigo Munhoz <126480629+rodmunhoz@users.noreply.github.com>
2023-04-20 22:30:31 -0300

    Create testeturtle.py

---
+++ testeturtle.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+raio = 5
+cor1 = "lime"
+cor2 = "white"
+cor = cor1
+bgcolor("darkgreen")
+
+for i in range(1, 8):
+  if cor == cor1:
+      cor = cor2
+  else:
+    cor = cor1
+  color(cor)
+  circle(raio*i)
+ 
